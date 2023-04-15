91a256580940b4abdf48f3ddf5ac4e0f12bb3502
canaler <robert.canale.work@gmail.com>
2023-04-15 18:40:16 +0800

    AddedReactApp

---
+++ client/src/components/Contacts.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import SingleContact from "./SingleContact";
+import AddContacts form "./AddContacts";
+
+export default class Contact extends Component{
+    constructor(props){
+    super(props);
+    this.state ={
+    contacts: [],
+    };
+    }
+
+    componentDidMount(){
+        fetch('http://localhost:8080/api/contacts')
+        .then(response => response.json())
+        .then(data => this.setState({contacts: data}))
+    }
+
+
+
+    render)(){
+        return (
+        <div>
+            <div className="row">
+                <AddContacts/>
+            </div>
+
+            <div className="row">
+                {this.state.contacts.map((item)=>
+                (<SingleContact key={item.id} item ={item}
+                )
+                )
+                }
+            </div>
+        </div>
+        )
+    }
+}
